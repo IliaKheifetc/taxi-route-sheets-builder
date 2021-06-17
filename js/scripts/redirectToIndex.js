7fd29b1ad1ab4e8b4564f59c9e9b4c979e134659
IliaKheifetc <kheyfetc.ilia@gmail.com>
2021-06-17 11:42:15 +0300

    fix infinite redirect

--- js/scripts/redirectToIndex.js
+++ js/scripts/redirectToIndex.js
@@ -1,2 +1,4 @@
-const newPathname = window.location.pathname.split("/").filter(Boolean)[0];
-window.location.pathname = `/${newPathname}/`;
+if (window.location.pathname.toLowerCase().includes("beelinetaxi")) {
+  const newPathname = window.location.pathname.split("/").filter(Boolean)[0];
+  window.location.pathname = `/${newPathname}/`;
+}
